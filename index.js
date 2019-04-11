8f55c1eec1c34e44d1721b8d3979e58d0e92ac53
Schrodinger <tencucsoc98@gmail.com>
2019-04-11 19:45:24 +0700

    hbs-0.2

--- index.js
+++ index.js
@@ -16,11 +16,33 @@ app.engine('hbs',hbs.engine);
 app.set('view engine','hbs');
 
 //define route
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get(['/','/home'],(req,res)=>{
+    res.render('home');
 });
 
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
 
+app.get('/consultant',(req,res)=>{
+    res.render('consultant');
+})
+
+app.get('/video',(req,res)=>{
+    res.render('video');
+})
+
+app.get('/question',(req,res)=>{
+    res.render('question');
+})
+
+app.get('/blog',(req,res)=>{
+    res.render('blog');
+})
+
+app.get('/you',(req,res)=>{
+    res.render('you');
+})
 
 //set server
 app.set('port', (process.env.PORT||5000));
